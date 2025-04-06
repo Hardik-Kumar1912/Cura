b1a9f20352c7afb52e05d9003de6269823a023ab
Hardik-Kumar1912 <hardikk1401@gmail.com>
2025-04-06 16:06:55 +0530

    favicon and preloader

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { AuthContextProvider } from './context/AuthContext.jsx'
 
+// Wait for full page load
+window.addEventListener("load", () => {
+  const preloader = document.getElementById("preloader");
+  if (preloader) {
+    // ⏳ Delay for 2 seconds before hiding (adjust as needed)
+    setTimeout(() => {
+      preloader.classList.add("hidden"); // fade out
+      setTimeout(() => {
+        preloader.remove(); // remove after transition
+      }, 500); // match fade-out duration in CSS
+    }, 2000); // 👈 2 seconds delay here
+  }
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthContextProvider>
